e4409f25bde0c1910c57241610d80800b9752c73
Bruno Velazquez <contacto.bruno.velazquez@gmail.com>
2021-12-06 09:37:10 -0600

    added animations to homepage

--- themes/MainTheme/assets/js/main.js
+++ themes/MainTheme/assets/js/main.js
@@ -1,1 +1,33 @@
-feather.replace();
+document.addEventListener("DOMContentLoaded", () => {
+    feather.replace();
+
+    const velazquezTextWrapper = document.querySelector('.bv-header-h2');
+    velazquezTextWrapper.innerHTML = velazquezTextWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({loop: true})
+        .add({
+            targets: '.bv-grid',
+            right: '-1000px',
+            easing: 'linear',
+            duration: 1000
+        });
+
+    anime.timeline({loop: false})
+        .add({
+            targets: '.bv-header-h2 .letter',
+            scale: [4,1],
+            opacity: [0,1],
+            translateZ: 0,
+            easing: "easeOutExpo",
+            duration: 1000,
+            delay: (el, i) => 70*i
+        })
+
+    anime.timeline({loop: false})
+        .add({
+            targets: '#bruno, #bruno-shadow',
+            rotateY: [-90, 0],
+            duration: 1500,
+            delay: (el, i) => 45 * i
+        })
+});
